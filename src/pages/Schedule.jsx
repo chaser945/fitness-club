6f54014274bd4952a63703ec602b1469a093a4e2
chaser945 <113222684+chaser945@users.noreply.github.com>
2023-08-10 14:08:53 +0500

    added weekdays

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,4 +1,34 @@
+import { Link } from "react-router-dom"
+import styled from "styled-components"
+import { PageHero, WeekButton } from "../components"
+import { weekDays } from "../utils"
+
 const Schedule = () => {
-  return <div>Schedule</div>
+  return (
+    <Wrapper>
+      <div className="schedule-wrapper">
+        <PageHero heading="schedule by day" />
+        <div className="days-wrapper">
+          {weekDays.map((c, index) => {
+            return (
+              <Link key={index} to={`/schedule/${c}`}>
+                <WeekButton weekDay={c} />
+              </Link>
+            )
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 export default Schedule
+
+const Wrapper = styled.div`
+  .days-wrapper {
+    padding: 4em 1em 2em 1em;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1em;
+    justify-content: center;
+  }
+`
